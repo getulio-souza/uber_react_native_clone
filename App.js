5707ff5a9bcd17a7cff293636e286fa93364e3c0
Getúlio Souza <getuliosouza@live.com>
2022-05-03 15:38:38 -0300

    organizando o projeto

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
+    <Provider store={}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Let's build Uber!</Text>
     </View>
+    </Provider>
   );
 }
 
